89c2f83f425b9a094e8161b591f791a738ed7ac8
Kylehanna <Kylehan215@gmail.com>
2023-01-25 09:38:26 -0600

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,22 @@
+let person = "Kyle Hanna";
+person
+let dayOfBirth = 16;
+console.log(dayOfBirth);
+person = "John"; 
+person
+// You get a warning message when using an existing name.
+// It does overwrite previous values, where the 2nd takes precedence. 
+
+// Get user input
+let color = "blue" // prompt("What is your favorite color?");
+//Display user input.
+console.log(`Thank you for sharing your favorite color: ${color}.`); 
+console.log('There isn\'t a "reason" we should\'ve gone over budget.')
+
+// A "Null" represents the intentional lack of a value or nothingness. (e.g. middle name usually not number)
+// "Undefined" represents a value that has not been defined.
+// "NaN" is a numeric value that represents a failed conversion. (e.g. "blue"/4)
+let thing 
+console.log(thing)
+
+
